17b4585fa375903d2241075f8d213b5dfd4cd8d8
Greenwools <green38162@gmail.com>
2025-09-29 22:59:46 +0900

    feat : 베이스 맵 프레임 + 스크립트 추가 및 개선(5주차)_수정_2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _menuUI;
+
+    private bool isPause = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,38 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPause) CloseMenu();
+
+            else OpenMenu();
+        }
+    }
+
+    public void OpenMenu()
+    {
+        _menuUI.SetActive(true);
+        Time.timeScale = 0f;        // 정지
+        isPause = true;
+        Cursor.lockState = CursorLockMode.None;     // 정지해도 마우스 커서 고정 해제
+        Cursor.visible = true;                      // 조작은 해야 하니 마우스 커서는 보이게
+    }
+
+    public void CloseMenu()
+    {
+        _menuUI.SetActive(false);
+        Time.timeScale = 1f;        // 정지 해제
+        isPause = false;
+        Cursor.lockState = CursorLockMode.Locked;   // 게임 시작 시, 마우스 커서 숨기기
+        Cursor.visible= false;
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
