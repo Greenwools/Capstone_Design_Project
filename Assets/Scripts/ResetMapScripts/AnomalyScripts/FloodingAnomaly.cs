f26253c6f2fbce119c65c531bb1a8474fdffb8b1
Greenwools <green38162@gmail.com>
2025-11-07 20:22:54 +0900

    feat : 물이 차오르는 연출 추가

--- Assets/Scripts/ResetMapScripts/AnomalyScripts/FloodingAnomaly.cs
+++ Assets/Scripts/ResetMapScripts/AnomalyScripts/FloodingAnomaly.cs
@@ -1,35 +1,50 @@
+using OccaSoftware.Buto.Runtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class FloodingAnomaly : MonoBehaviour, IAnomaly, IResetable
 {
     private PlayerController _playerController;
     private bool _anomalyTriggered = false;
-    private Material _waterMaterial;
-    private float _initialWaterLevel;
+    private Vector3 _initialWaterPosition;
+    private LensDistortion _lensDistortion;
+    private Coroutine _wobbleCoroutine;
 
-    public GameObject WaterVolume;
-    public float DrownWaterHeight = 3.0f;
+    public GameObject WaterVolumeObject;
+    public Volume PostProcessVolume;
+    public Image WaterOverlayUI;
+    public Image FadeImage;
+    public Transform PlayerTransform;
+    public Transform SpawnTransform;
+
+    public float TargetWaterHeight = 3.0f;
     public float TimeToDrown = 30.0f;
+    public float WobbleSpeed = 2f;
+    public float WobbleIntensity = 0.3f;
 
     public AudioSource WaterAudio;
     public AudioSource BreathingAudio;
     public AudioSource HeartBeatAudio;
+    public AudioSource DrownAudio;
 
     void Awake()
     {
         GetComponent<Collider>().isTrigger = true;
         _playerController = FindObjectOfType<PlayerController>();
 
-        if (WaterVolume != null) 
-        { 
-            Renderer waterRenderer = WaterVolume.GetComponent<Renderer>();
-            _waterMaterial = waterRenderer.material;
+        if (PlayerTransform == null) PlayerTransform = _playerController.transform;
+        if (SpawnTransform == null) SpawnTransform = GameObject.Find("SpawnPoint").transform;
+        if (FadeImage == null) FadeImage = GameObject.Find("FadeImage").GetComponent<Image>();
+        if (WaterOverlayUI == null) WaterOverlayUI = GameObject.Find("WaterOverlay").GetComponent<Image>();
+        if (PostProcessVolume == null) PostProcessVolume = FindObjectOfType<Volume>();
 
-            _initialWaterLevel = -1f;
-            _waterMaterial.SetFloat("_waterLevel", _initialWaterLevel);
-        }
+        if (WaterVolumeObject != null) _initialWaterPosition = WaterVolumeObject.transform.position;
+
+        if (PostProcessVolume != null) PostProcessVolume.profile.TryGet(out _lensDistortion);
 
         if (WaterAudio == null) WaterAudio = GetComponent<AudioSource>();
         WaterAudio.playOnAwake = false;
@@ -48,16 +63,20 @@ public class FloodingAnomaly : MonoBehaviour, IAnomaly, IResetable
     public void ResetState()
     {
         StopAllCoroutines();
+        if (_wobbleCoroutine != null) StopCoroutine(_wobbleCoroutine);
 
         WaterAudio.Stop();
         if (BreathingAudio != null) BreathingAudio.Stop();
         if (HeartBeatAudio != null) HeartBeatAudio.Stop();
 
-        if (WaterVolume != null)
+        if (WaterVolumeObject != null)
         {
-            _waterMaterial.SetFloat("_waterLevel", _initialWaterLevel);
-            WaterVolume.SetActive(false);
+            WaterVolumeObject.transform.position = _initialWaterPosition;
+            WaterVolumeObject.SetActive(false);
         }
+
+        if (WaterOverlayUI != null) WaterOverlayUI.gameObject.SetActive(false);
+        if (_lensDistortion != null) _lensDistortion.intensity.value = 0f;
 
         if (_playerController != null) _playerController.IsInWater = false;
         _anomalyTriggered = false;
@@ -70,6 +89,7 @@ public class FloodingAnomaly : MonoBehaviour, IAnomaly, IResetable
         {
             _anomalyTriggered = true;
             StartCoroutine(FloodSequence());
+            StartCoroutine(DrownTimer());
         }
     }
 
@@ -81,25 +101,102 @@ public class FloodingAnomaly : MonoBehaviour, IAnomaly, IResetable
 
         if (_playerController != null) _playerController.IsInWater = true;
 
-        if (WaterVolume != null)
+        if (WaterVolumeObject != null)
         {
-            WaterVolume.SetActive(true);
-            float currentHeight = _initialWaterLevel;
+            WaterVolumeObject.SetActive(true);
+            Vector3 startPosition = _initialWaterPosition;
+            Vector3 targetPosition = new Vector3(_initialWaterPosition.x, TargetWaterHeight, _initialWaterPosition.z);
             float elapsedTime = 0f;
 
-            while (elapsedTime < TimeToDrown) 
+            Transform cameraTransform = _playerController.GetComponentInChildren<Camera>().transform;
+            bool submerged = false;
+
+            while (elapsedTime < TimeToDrown)
             {
-                currentHeight = Mathf.Lerp(_initialWaterLevel, DrownWaterHeight, elapsedTime / TimeToDrown);
+                WaterVolumeObject.transform.position = Vector3.Lerp(startPosition, targetPosition, elapsedTime / TimeToDrown);
 
-                _waterMaterial.SetFloat("_waterLevel", currentHeight);
+                float currentWaterSurfaceY = WaterVolumeObject.transform.position.y + (WaterVolumeObject.transform.localScale.y / 2f);
 
-                if (_playerController != null) _playerController.CurrentWaterLevel = currentHeight;
+                if (_playerController != null) _playerController.CurrentWaterLevel = currentWaterSurfaceY;
+
+                if (!submerged && cameraTransform.position.y + 0.25f < currentWaterSurfaceY)
+                {
+                    submerged = true;
+                    if (WaterOverlayUI != null) WaterOverlayUI.gameObject.SetActive(true);
+                    if (BreathingAudio != null) BreathingAudio.Stop();
+                    if (_lensDistortion != null) _wobbleCoroutine = StartCoroutine(WobbleEffect());
+                }
 
                 elapsedTime += Time.deltaTime;
+
                 yield return null;
             }
-
-            _waterMaterial.SetFloat("_waterLevel", DrownWaterHeight);
         }
+    }
+
+    private IEnumerator WobbleEffect()
+    {
+        float noiseOffsetX = Random.Range(0f, 100f);
+        float noiseOffsetY = Random.Range(0f, 100f);
+
+        while (true)
+        {
+            float noise = (Mathf.PerlinNoise(noiseOffsetX + Time.time * WobbleSpeed, noiseOffsetY) * 2f) - 1f;
+
+            _lensDistortion.intensity.value = noise * WobbleIntensity;
+            yield return null;
+        }
+    }
+
+    private IEnumerator DrownTimer()
+    {
+        yield return new WaitForSeconds(TimeToDrown);
+        Debug.Log("시간 초과, 이상 현상 돌파 실패");
+
+        GameManager.IsPlayerStop = true;
+
+        WaterAudio.Stop();
+        if (BreathingAudio != null) BreathingAudio.Stop();
+        if (HeartBeatAudio != null) HeartBeatAudio.Stop();
+        if (DrownAudio != null) DrownAudio.Play();
+        if (WaterOverlayUI != null) WaterOverlayUI.gameObject.SetActive(false);
+        if (_wobbleCoroutine != null) StopCoroutine(_wobbleCoroutine);
+        if (_lensDistortion != null) _lensDistortion.intensity.value = 0f;
+
+        DecreaseSanity();
+
+        float timer = 0f;
+        float fadeDuration = 1.5f;
+        while (timer < fadeDuration)
+        {
+            FadeImage.color = new Color(0, 0, 0, timer / fadeDuration);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        FadeImage.color = new Color(0, 0, 0, 1);
+
+        CharacterController cc = PlayerTransform.GetComponent<CharacterController>();
+        cc.enabled = false;
+        PlayerTransform.position = SpawnTransform.position;
+        PlayerTransform.rotation = SpawnTransform.rotation;
+        cc.enabled = true;
+
+        yield return new WaitForSeconds(0.5f);
+        timer = 0f;
+        while (timer < fadeDuration)
+        {
+            FadeImage.color = new Color(0, 0, 0, 1 - (timer / fadeDuration));
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        FadeImage.color = new Color(0, 0, 0, 0);
+
+        GameManager.Instance.DecideNextLoopState();
+        GameManager.IsPlayerStop = false;
+    }
+
+    private void DecreaseSanity()
+    {
+
     }
 }
